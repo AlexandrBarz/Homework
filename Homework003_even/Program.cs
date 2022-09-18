cb0dd84b5680a9913f295749b9032e7c1ece24e3
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-18 18:06:08 +0300

    added Homework003 even

---
+++ Homework003_even/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Программа проверки четности числа");
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число " + a + " является четным");
+}
+else
+{
+    Console.WriteLine("Число " + a + " является нечетным");
+}
