c45aa2107751fb55fa3c7fbbec3a7c69aac951a3
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-18 17:13:42 +0300

    Homework002 max from 3 numbers

---
+++ Homework002_max_from_3_numbers/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите 3 числа для сравнения");
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+Console.WriteLine("Максимальное число: " + max);
