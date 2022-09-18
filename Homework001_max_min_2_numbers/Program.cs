f45e94e47cf6e108a68a7efbd2a59588452dce69
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-18 16:48:06 +0300

    added Homework001_max_min_2_numbers and rename file

---
+++ Homework001_max_min_2_numbers/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите 2 числа для сравнения");
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("Число " + a + " больше чем " + b);
+}
+else
+{
+    Console.WriteLine("Число " + b + " больше чем " + a);
+}
