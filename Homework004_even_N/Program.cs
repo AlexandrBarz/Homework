f396d5285a228d484e88c45327da5966ffbdedcd
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-19 09:42:59 +0300

    corrected Homework004 even N

--- Homework004_even_N/Program.cs
+++ Homework004_even_N/Program.cs
@@ -1,7 +1,13 @@
 ﻿Console.WriteLine("Введите число N для нахождения четных чисел от 1 до N");
-int N=int.Parse(Console.ReadLine());
-int number=1;
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Четные числа в ряду от 1 до " + N);
+int number = 1;
 
-while(number<=N)
-{
-    
+while (number <= N)
+{   
+    if (number % 2 == 0)
+    {
+        Console.Write(number + " ");
+    }
+    number++;
+}
