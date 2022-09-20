eb777a21b6c08cacbcf42b3e7eda0754ba9a15ef
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-20 21:16:39 +0300

    added a condition checking a negativ numbers in Homework 005 006 007

--- Homework005_006_007/Program.cs
+++ Homework005_006_007/Program.cs
@@ -15,14 +15,30 @@
 void Third_Digit()
 {
     Console.WriteLine("Введите число: ");
-    string number_text = Convert.ToString(Console.ReadLine());
-    if (number_text.Length > 2)
+    int natural_number = int.Parse(Console.ReadLine());
+    if (natural_number >= 0)
     {
-        Console.WriteLine("Третья цифра введенного числа: " + number_text[2]);
+        string number_text = Convert.ToString(natural_number);
+        if (number_text.Length > 2)
+        {
+            Console.WriteLine("Третья цифра введенного числа: " + number_text[2]);
+        }
+        else
+        {
+            Console.WriteLine("У введеного числа нет третьей цифры.");
+        }
     }
     else
     {
-        Console.WriteLine("У введеного числа нет третьей цифры.");
+        string number_text = Convert.ToString(natural_number);
+        if (number_text.Length > 3)
+        {
+            Console.WriteLine("Третья цифра введенного числа: " + number_text[3]);
+        }
+        else
+        {
+            Console.WriteLine("У введеного числа нет третьей цифры.");
+        }
     }
 }
 
@@ -47,24 +63,22 @@ void Day_Off()
     }
 }
 
-Console.WriteLine("Введите номер задания:");
+Console.WriteLine("Введите номер задания 1, 2 или 3:");
 while (true)
 {
     int exercise = int.Parse(Console.ReadLine());
     if (exercise == 1)
     {
         Second_number();
-
     }
     if (exercise == 2)
     {
         Third_Digit();
-
     }
     if (exercise == 3)
     {
         Day_Off();
-
     }
-return ;
+    Console.WriteLine("Такого задания нет в списке. Выберите задание из списка.");
+    return;
 }
