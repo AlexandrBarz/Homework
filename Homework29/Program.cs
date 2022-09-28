4dee590d4fc32f8198fbf57d36c6e6d9e3ed482f
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-28 23:15:59 +0300

    added Homework 29

---
+++ Homework29/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
