8443eb9fa5d594701ed24e7672b5c70e71281974
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-28 19:05:25 +0300

    added Homework 25

---
+++ Homework25/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int ReadData(string msg)
+{
+    Console.Write(msg);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+int PowAtoB(int a, int b)
+{
+    int result = (int)Math.Pow(a, b);
+    return result;
+}
+
+void PrintData(string msg, int value)
+{
+    Console.WriteLine(msg + value);
+}
+
+int numberA = ReadData("Ввведите число А: ");
+int numberB = ReadData("Ввведите число B: ");
+int result = PowAtoB(numberA, numberB);
+PrintData("Итог возведения числа " + numberA + " в степень " + numberB + " равен ", result);
