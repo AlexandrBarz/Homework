e85b5469d1d8c8d32eb6d87b5a3723c69eb42e6a
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-28 23:44:12 +0300

    corrected Homework 29

--- Homework29/Program.cs
+++ Homework29/Program.cs
@@ -5,3 +5,27 @@ int ReadData(string msg)
     int number = int.Parse(Console.ReadLine() ?? "0");
     return number;
 }
+int[] FillArr(int len, int lowbord, int highbord)
+{
+    int[] array = new int[len];
+    for (int i = 0; i < len; i++)
+    {
+        array[i] = new Random().Next(lowbord, highbord + 1);
+    }
+    return array;
+}
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + "]");
+}
+
+int arrlen = ReadData("Введите длину массива: ");
+int lowbord = ReadData("Введите нижнюю границу чисел: ");
+int highbord = ReadData("Введите верхнюю границу чисел: ");
+int[] arr = FillArr(arrlen, lowbord, highbord);
+PrintArr(arr);
