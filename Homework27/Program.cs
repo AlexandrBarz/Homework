834c96df09d1ba9893323d525ca54d099c3fcc97
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-28 23:15:46 +0300

    corrected Homework 27

--- Homework27/Program.cs
+++ Homework27/Program.cs
@@ -11,13 +11,27 @@ void PrintData(string msg, int value)
     Console.WriteLine(msg + value);
 }
 
+//int number = ReadData("Введите число: ");
+//int sum = 0;
+//if (number < 0) number *= -1;
+//while (number > 0)
+//{
+//    int res = number % 10;
+//    sum = sum + res;
+//    number = number / 10;
+//}
+//PrintData("Сумма чисел введенного числа = ", sum);
+
+//Тоже решение только с использованием строки
+
 int number = ReadData("Введите число: ");
-int sum = 0;
-if (number < 0) number *= -1;
-while (number > 0)
-{
-    int res = number % 10;
-    sum = sum + res;
-    number = number / 10;
-}
-PrintData("Сумма чисел введенного числа = ", sum);
+//int sum = 0;
+string str = number.ToString();
+string[] strarr = new string[str.Length];
+int[] ints = Array.ConvertAll(strarr, int.Parse);
+Console.WriteLine(ints[1]);
+//int[] intiger = Array.ConvertAll(strnum, int.Parse);
+//for(int i=0;i<arrnum.Length-1;i++)
+//sum=sum+arrnum[i];
+//Console.WriteLine(strnum);
+//PrintData("Сумма чисел введенного числа = ", strnum);
