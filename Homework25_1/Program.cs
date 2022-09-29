d7b64020f43f624982c50a3a4a66ec3e6d5a9c2e
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-09-29 03:03:33 +0300

    added Homework 25_1

---
+++ Homework25_1/Program.cs
@@ -0,0 +1,57 @@
+﻿// Написать калькулятор с операциями +, -, /, * и возведение в степень
+double ReadData(string msg)
+{
+    Console.Write(msg);
+    double number = double.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+Console.WriteLine(@"Выбирете арифмитическую операцию:
+    +  сложение
+    -  вычитание
+    *  умножение
+    /  деление
+    ^  возведение в степень");
+string oper = Console.ReadLine();
+if (oper != "+" && oper != "-" && oper != "*" && oper != "/" && oper != "^")
+{
+    Console.WriteLine("Данной операции нет в калькуляторе.");
+}
+else
+{
+    double c = 0;
+    double a = ReadData("Введите первое число: ");
+    double b = ReadData("Введите второе число: ");
+    if (oper == "+")
+    {
+        Console.WriteLine($"Результат арифметической операции: {a} {oper} {b} = {a + b}");
+    }
+    else
+    {
+        if (oper == "-")
+        {
+            Console.WriteLine($"Результат арифметической операции: {a} {oper} {b} = {a - b}");
+        }
+        else
+        {
+            if (oper == "*")
+            {
+                Console.WriteLine($"Результат арифметической операции: {a} {oper} {b} = {a * b}");
+            }
+            else
+            {
+                if (oper == "/")
+                {
+                    Console.WriteLine($"Результат арифметической операции: {a} {oper} {b} = {a / b}");
+                }
+                else
+                {
+                    if (oper == "^")
+                    {
+                        Console.WriteLine($"Результат арифметической операции: {a} ^ {b} = {Math.Pow(a, b)}");
+                    }
+                }
+            }
+        }
+    }
+}
