f412eb64f7e99b0f7400153c6decfd24d2ae5f59
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-10-04 01:46:17 +0300

    added Homework 43

---
+++ Homework043/Program.cs
@@ -0,0 +1,20 @@
+﻿//  Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+Console.Clear();
+Console.WriteLine(@"Нахождение точки пересечения 2-ух прямых, заданных уравнениями,
+            y = k1 * x + b1
+            y = k2 * x +b2");
+Console.WriteLine();
+Console.Write("Введите значение b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+//double xpoint = 0;
+//double ypoint = 0;
+double xpoint = (b2 - b1) / (k1 - k2);
+double ypoint = k1 * xpoint + b1;
+Console.WriteLine($"Координаты точки пересечения 2-х прямых {xpoint}, {ypoint}");
+Console.ReadKey();
