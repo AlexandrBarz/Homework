51d41e24742f09847c1f0ae37bebbeee2c0e942b
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-10-04 01:09:38 +0300

    added Homework 41

---
+++ Homework041/Program.cs
@@ -0,0 +1,9 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите числа через пробел: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0) count++;
+}
+Console.WriteLine($"Количество положительных чисел = {count}");
