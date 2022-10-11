1782b96eb7df69853d5d0ea762df5e0d82fa0a18
Alexandr Barzanov <Alexandrbarzanov@gmail.com>
2022-10-11 17:50:11 +0300

    corrected Homework 54

--- Homework054/Program.cs
+++ Homework054/Program.cs
@@ -5,7 +5,7 @@ int ReadData(string msg)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-int[,] GetArray(int row, int column, int lowbord, int highbord)
+int[,] FillArray(int row, int column, int lowbord, int highbord)
 {
     int[,] array = new int[row, column];
     for (int i = 0; i < row; i++)
@@ -13,37 +13,48 @@ int[,] GetArray(int row, int column, int lowbord, int highbord)
         for (int j = 0; j < column; j++)
         {
             array[i, j] = new Random().Next(lowbord, highbord + 1);
-            Console.Write(array[i, j] + "\t");
         }
-        Console.WriteLine();
     }
     return array;
 }
 
-int[] SortArray(int[] matrix, int[] array)
+void PrintArray(int[,] array)
 {
-    int[] outs = new int[array.Length];
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int y = 0; y < matrix.GetLength(1); y++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            for (int x = 0; x < matrix.GetLength(0); x++)
-            {
-                array[i] = matrix[x, y];
-                outs[i]++;
-            }
-            //     Array.Sort(array);
-            //     Array.Reverse(array);
-            //     Console.WriteLine(outs[i]);
+            Console.Write($"{array[i, j]}\t");
         }
-        Console.WriteLine(outs[i]);
+        Console.WriteLine();
     }
-    return outs;
+}
+
+void SortArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
 }
 
 int rows = ReadData("Введите кол-во строк в массиве: ");
 int columns = ReadData("Введите кол-во столбцов в массиве: ");
 int lowbord = ReadData("Введите нижнюю границу диапазона чисел для заполнения массива: ");
 int highbord = ReadData("Введите верхнюю границу диапазона чисел для заполнения массива: ");
-int[,] newarray = GetArray(rows, columns, lowbord, highbord);
-int[] rowarray = SortArray(newarray);
+int[,] newarray = FillArray(rows, columns, lowbord, highbord);
+PrintArray(newarray);
+SortArray(newarray);
+Console.WriteLine();
+PrintArray(newarray);
